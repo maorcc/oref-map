781e407de98578e6c7755426e2bdd0f83b5b7c83
Uri Pe'er <uri.peer3@gmail.com>
2026-03-14 23:47:37 +0200

    pr

--- functions/api/alarms-history.js
+++ functions/api/alarms-history.js
@@ -4,17 +4,19 @@ export async function onRequestGet(context) {
   const url = new URL(context.request.url);
   const fromDate = url.searchParams.get('fromDate');
   const toDate = url.searchParams.get('toDate');
-
-  // let targetUrl = 'http://127.0.0.1:5000/Shared/Ajax/GetAlarmsHistory.aspx?lang=he&mode=0';
-  let targetUrl = 'https://alerts-history.oref.org.il/Shared/Ajax/GetAlarmsHistory.aspx?lang=he&mode=0';
-
+  // let target = 'http://127.0.0.1:5000/Shared/Ajax/GetAlarmsHistory.aspx';
+  let target = 'https://alerts-history.oref.org.il/Shared/Ajax/GetAlarmsHistory.aspx'
+  const targetUrl = new URL(target)
+  targetUrl.searchParams.set('lang', 'he');
+  targetUrl.searchParams.set('mode', '0');
   if (fromDate && toDate) {
-    targetUrl += `&fromDate=${fromDate}&toDate=${toDate}`;
+    targetUrl.searchParams.set('fromDate', fromDate);
+    targetUrl.searchParams.set('toDate', toDate);
   }
 
   return orefProxy(context, {
-    target: targetUrl,
-    redirectSuffix: '/api2/alarms-history',
+    target: targetUrl.toString(),
+      redirectSuffix: '/api2/alarms-history',
     kind: 'alarms-history',
   });
 }
